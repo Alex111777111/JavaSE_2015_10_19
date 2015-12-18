049c7d4600ae76eab3742e5e5b3140ad470cc3e2
sbt-saburov-ag <sbt-saburov-ag@sberbank.ru>
2015-12-17 23:15:34 -0500

    Part 18

---
+++ JspExample1/web/index.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Start Page</title>
+    </head>
+    <body>
+        <a href="controller">JSP Example</a>
+    </body>
+</html>
---
+++ WebExample1/src/java/edu/javacourse/web/FirstServlet.java
@@ -0,0 +1,27 @@
+package edu.javacourse.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String name = request.getParameter("name");
+        if(name == null) {
+            name = "Stranger";
+        }
+        
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.append("<strong>Hello, " + name + "!!!</strong>");
+        writer.close();
+    }
+
+}
